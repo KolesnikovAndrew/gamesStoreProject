f2fdb15b5ea71b945c6ee004aab72c9a45e3b569
KolesnikovAndrew <andrejs.kolesnikovs.tsi@inbox.lv>
2022-04-13 20:41:50 +0300

    changed mobile version styling

--- src/components/Pages/GamesList/GameList.js
+++ src/components/Pages/GamesList/GameList.js
@@ -3,10 +3,10 @@ import Paginator from "../../utils/Paginator/Paginator";
 import Game from "./Game/Game";
 import Searchbar from "../../Searchbar/Searchbar";
 import GamesCarousel from "../../utils/Carousel/Carousel";
-import styles from "./GameList.module.scss"
+import styles from "./GameList.module.scss";
 
-export const GameList = ({ games, setCount, setProduct, product  }) => {
-
+export const GameList = ({ games, setCount, setProduct, product }) => {
+  console.log(games);
   //Pagination
   const [currentPage] = useState(1);
   const [productsPerPage, setProductsPerPage] = useState(15);
@@ -20,37 +20,42 @@ export const GameList = ({ games, setCount, setProduct, product  }) => {
   };
 
   //Filter games
-  const [filterText, setFilterText] = useState('')
-  const [filterGenre, setFIlterGenre] = useState('')
+  const [filterText, setFilterText] = useState("");
+  const [filterGenre, setFIlterGenre] = useState("");
 
-  const filteredGames = 
-    games
-      .filter(game => game.title.toLowerCase().match(filterText.toLowerCase()))
-      .filter(game => game.genre.toLowerCase().match(filterGenre.toLowerCase()))
+  const filteredGames = games
+    .filter((game) => game.title.toLowerCase().match(filterText.toLowerCase()))
+    .filter((game) =>
+      game.genre.toLowerCase().match(filterGenre.toLowerCase())
+    );
 
   return (
-    
     <div>
-      <div className={styles.carouselContainer} >
-        <GamesCarousel/>
+      <div className={styles.carouselContainer}>
+        <GamesCarousel />
       </div>
-      
-      <Searchbar setFIlterGenre={setFIlterGenre} setFilterText={setFilterText}/>
+
+      <Searchbar
+        setFIlterGenre={setFIlterGenre}
+        setFilterText={setFilterText}
+      />
       <div>
-      {filteredGames.slice(indexOfFirstProduct, indexOfLastProduct).map((g) => (
-          <Game
-            key={g.id}
-            title={g.title}
-            thumbnail={g.thumbnail}
-            description={g.short_description}
-            genre={g.genre}
-            platform={g.platform}
-            id={g.id}
-            setCount={setCount}
-            setProduct={setProduct}
-            product={product}
-          />
-      ))} 
+        {filteredGames
+          .slice(indexOfFirstProduct, indexOfLastProduct)
+          .map((g) => (
+            <Game
+              key={g.id}
+              title={g.title}
+              thumbnail={g.thumbnail}
+              description={g.short_description}
+              genre={g.genre}
+              platform={g.platform}
+              id={g.id}
+              setCount={setCount}
+              setProduct={setProduct}
+              product={product}
+            />
+          ))}
       </div>
       <Paginator
         productsPerPage={productsPerPage}
--- src/components/Pages/Product/Product.js
+++ src/components/Pages/Product/Product.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import AddToCart from '../../utils/AddToCart/AddToCart';
+import React from "react";
+import { useSelector } from "react-redux";
+import AddToCart from "../../utils/AddToCart/AddToCart";
 
-export const Product = ({addEntry, count}) => {
-  let price = (Math.random()*100).toFixed(2)
-  let selectedGame = []
-  const games = useSelector(state => state.shop.games)
-  for(let i=0;i < games.length; i++) {
-    if(games[i].id == count) {
+import styles from "./Product.module.scss";
+
+export const Product = ({ addEntry, count }) => {
+  let price = (Math.random() * 100).toFixed(2);
+  let selectedGame = [];
+  const games = useSelector((state) => state.shop.games);
+  for (let i = 0; i < games.length; i++) {
+    if (games[i].id == count) {
       selectedGame = games[i];
-    };
+      console.log(selectedGame);
+    }
   }
-  
-  return (
-  <div>
-    <h1>{selectedGame.title}</h1>
-    <img src={selectedGame.thumbnail}></img>
-    <h1>{selectedGame.title}</h1>
-    <p >{selectedGame.description}</p>
-    <p>Genres: {selectedGame.genre}</p>
-    <p>{selectedGame.platform == 'PC (Windows)'? 'Windows': 'Platforms'}</p>
-    <p>{price}</p>
-    {<AddToCart price={price} title={selectedGame.title} platform={selectedGame.platform} thumbnail={selectedGame.thumbnail} addEntry={addEntry}/>}
-  </div>
-  )
-}
 
-export default Product
+  return (
+    <div className={styles.product}>
+      <img src={selectedGame.thumbnail}></img>
+      <h1>{selectedGame.title}</h1>
+      <p>Publisher:{selectedGame.publisher}</p>
+      <p>Realease date:{selectedGame.release_date}</p>
+      <p>{selectedGame.short_description}</p>
+      <p>Genres: {selectedGame.genre}</p>
+      <p>{selectedGame.platform == "PC (Windows)" ? "Windows" : "Platforms"}</p>
+      <p>{price}</p>
+      {
+        <AddToCart
+          price={price}
+          title={selectedGame.title}
+          platform={selectedGame.platform}
+          thumbnail={selectedGame.thumbnail}
+          addEntry={addEntry}
+        />
+      }
+    </div>
+  );
+};
+
+export default Product;
