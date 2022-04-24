65dab8a11e74a9355112c3e5be79a9a48b59ed12
KolesnikovAndrew <andrejs.kolesnikovs.tsi@inbox.lv>
2022-04-25 01:39:50 +0300

    moved total cost counter to center from leftside, as well as changed it's color

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
-import {AiOutlineArrowDown, AiOutlineArrowUp} from 'react-icons/ai'
-import styles from "./Searchbar.module.scss"
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
+import styles from "./Searchbar.module.scss";
 
+const genres = ["Shooter", "MMORPG", "Card Game"];
 
-const genres = ['Shooter', 'MMORPG', 'Card Game']
-
-export const Searchbar = ({setFilterText, setFIlterGenre}) => {
-  const [moreOptions, setMoreOptions] = useState(false)
-  const getFilterData = (e) =>{setFilterText(e.target.value)}
-  const getFilterGenre = (e) =>{
-    e.target.checked ? setFIlterGenre(e.target.value) : setFIlterGenre('')
-  }
+export const Searchbar = ({ setFilterText, setFIlterGenre }) => {
+  const [moreOptions, setMoreOptions] = useState(false);
+  const getFilterData = (e) => {
+    setFilterText(e.target.value);
+  };
+  const getFilterGenre = (e) => {
+    e.target.checked ? setFIlterGenre(e.target.value) : setFIlterGenre("");
+  };
   return (
     <div className={styles.searchBar}>
       <div className="container h-100">
@@ -28,30 +29,51 @@ export const Searchbar = ({setFilterText, setFIlterGenre}) => {
             aria-describedby="basic-addon2"
           />
         </InputGroup>
-        <p className={styles.moreOptions} onClick={()=> setMoreOptions(!moreOptions)}>
-          <span>{moreOptions? <>Hide options<AiOutlineArrowUp/></> : <>More options<AiOutlineArrowDown/></>}</span>
+        <p
+          className={styles.moreOptions}
+          onClick={() => setMoreOptions(!moreOptions)}
+        >
+          <span>
+            {moreOptions ? (
+              <>
+                Hide options
+                <AiOutlineArrowUp />
+              </>
+            ) : (
+              <>
+                More options
+                <AiOutlineArrowDown />
+              </>
+            )}
+          </span>
         </p>
       </div>
-      { moreOptions ? 
-        <div className={styles.searchOptions} >
-            <h3>Genres:</h3>
+      {moreOptions ? (
+        <div className={styles.searchOptions}>
+          <h3>Genres:</h3>
           <ul>
-            {
-              genres.map((genre)=>{
-                return <>
+            {genres.map((genre) => {
+              return (
+                <>
                   <li>
-                    <input type="checkbox" id={genre} name={genre} value={genre} onChange={getFilterGenre}/>
+                    <input
+                      type="checkbox"
+                      id={genre}
+                      name={genre}
+                      value={genre}
+                      onChange={getFilterGenre}
+                    />
                     <label for={genre}> {genre}</label>
                   </li>
                 </>
-              })
-            }
+              );
+            })}
           </ul>
+          <h3></h3>
         </div>
-       : null
-      }
-      </div>
-  )
-}
+      ) : null}
+    </div>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
